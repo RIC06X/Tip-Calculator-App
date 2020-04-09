963581d4e752af77bb0364838d85a468ccf69b2f
Yanjie Xu <yanjiexu06@gmail.com>
2020-04-09 22:38:57 +0800

    Initial Commit

---
+++ Tip Calculator App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator App
+//
+//  Created by Yanjie Xu on 2020/4/9.
+//  Copyright © 2020 Yanjie Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
