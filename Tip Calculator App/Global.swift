7f76617e9e100f37addc74cc21415eb5e2741922
Yanjie Xu <yanjiexu06@gmail.com>
2020-04-12 23:06:33 +0800

    Added settings view controller

---
+++ Tip Calculator App/Global.swift
@@ -0,0 +1,12 @@
+//
+//  Global.swift
+//  Tip Calculator App
+//
+//  Created by Yanjie Xu on 2020/4/12.
+//  Copyright © 2020 Yanjie Xu. All rights reserved.
+//
+
+import Foundation
+
+var tips_defaults = UserDefaults.standard
+
